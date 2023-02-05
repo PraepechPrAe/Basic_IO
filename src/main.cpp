5f0550a60e1afd6380185728c8e7603b61c0f994
PrAe <praepech.t@gmail.com>
2023-02-05 21:40:46 +0700

    Final

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,6 @@ void setup() {
     Serial.begin(115200);
     debouncer.attach(BUTTON, INPUT_PULLUP);
     debouncer.interval(25); 
-    pinMode(GREEN, OUTPUT);
-    digitalWrite(GREEN,0);
-    pinMode(RED, OUTPUT);
-    digitalWrite(RED,0);
-    pinMode(YELLOW, OUTPUT);
-    digitalWrite(YELLOW,0);
-
     ledcSetup(0, 5000, 8);
     ledcSetup(1, 5000, 8);
     ledcSetup(2, 5000, 8);
@@ -28,6 +21,8 @@ void setup() {
 
 void loop()
 {
+  // Serial.println(analogRead(LDR));
+  // delay(100);
   debouncer.update();
   if ( debouncer.fell() ) { 
       cnt++;
@@ -37,7 +32,44 @@ void loop()
   for(int i=0; i<3; i++)
   {
     int a=num%2;
-    ledcWrite(i, a*(map(analogRead(LDR),2000,4000,0,255)));
+    ledcWrite(i, a*(map(analogRead(LDR),600,2000,0,255)));
     num = num/2;
   }
 }
+
+//Solution by P'Champ
+// #include <Bounce2.h>
+// #define RED 26
+// #define YELLOW 25 
+// #define GREEN 33
+// #define LDR 32
+// #define BUTTON 27
+// Bounce b = Bounce();
+// int cnt = 0;
+// void setup() {
+//   b.attach(BUTTON,INPUT_PULLUP);
+//   b.interval(25); 
+//   ledcSetup(0, 5000, 8);
+//   ledcSetup(1, 5000, 8);
+//   ledcSetup(2, 5000, 8);
+//   ledcAttachPin(RED, 0);
+//   ledcAttachPin(YELLOW, 1);
+//   ledcAttachPin(GREEN, 2);
+//   ledcAttachPin(5, 2);
+//   Serial.begin(115200);
+//   Serial.println("Hello, ESP32!");
+// }
+
+// void loop() {
+//   // Serial.println(analogRead(LDR));
+//   // delay(100);
+//   int x = map(analogRead(LDR), 1000, 4095, 0, 255);
+//   b.update();
+//   if (b.rose()){
+//       cnt++;
+//       Serial.println(cnt);
+//   }
+//   ledcWrite(0, ((cnt & 4) == 4) ? x : 0);
+//   ledcWrite(1, ((cnt & 2) == 2) ? x : 0);
+//   ledcWrite(2, ((cnt & 1) == 1) ? x : 0);
+// }
